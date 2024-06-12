a54c53390e3c2896efbefec13967a0aefdffce3e
Preston Chapman <pchapman82070@uat.edu>
2024-06-12 12:42:41 -0700

    Create Goodbye page

--- script/delete.js
+++ script/delete.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const FORM = document.getElementById("deleteForm");
     FORM.addEventListener("submit", function(e) {
+        e.preventDefault();
         alert("Form submitted");
+        window.location.pathname = "goodbye.html";
     })
 });
