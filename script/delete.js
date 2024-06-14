3382f3c6950a9f71607dcd8bce94a551594b91c5
Preston Chapman <pchapman82070@uat.edu>
2024-06-14 09:05:42 -0700

    Return if form is null
    If form does not exist stop function

--- script/delete.js
+++ script/delete.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const FORM = document.getElementById("deleteForm");
+    if(!FORM) return;
     FORM.addEventListener("submit", function(e) {
         e.preventDefault();
         alert("Form submitted");
